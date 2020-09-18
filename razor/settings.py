a9afc63004a98fcd4bea3a0780dc1e3fec51d1b6
boxabhi <abhijeetg40@gmail.com>
2020-09-18 21:06:13 +0530

    host

--- razor/settings.py
+++ razor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+!d#u#!=a*%+oxbp4^_+7c^7c(veo!1hx)+qq(#7yw3v=8r6jy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
